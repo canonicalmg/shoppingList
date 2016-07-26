11b0d9c7857f3898c2584fe9124e4e5077277df9
Marcus <marcusg@bixly.com>
2016-07-26 13:56:16 -0700

    android post

--- shoppingListApp/admin.py
+++ shoppingListApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import list, listEntry, profile
+from .models import list, listEntry, profile,textMessage
 # Register your models here.
 
 admin.site.register(list)
 admin.site.register(listEntry)
 admin.site.register(profile)
+admin.site.register(textMessage)
